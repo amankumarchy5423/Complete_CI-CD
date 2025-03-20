e1104f4e6073a0f11084bd34026784494ef9bd39
aman <amankumarchy5423@gmail.com>
2025-03-20 16:23:55 +0530

    some changeg

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
 # src/__init__.py
+
 PACKAGE_NAME = "src"
 VERSION = "1.0.0"
+
--- test/test_app.py
+++ test/test_app.py
@@ -4,4 +4,7 @@ def test_home():
     response = app.test_client().get()
 
     assert response.status_code == 200
+<<<<<<< HEAD
     
+=======
+>>>>>>> a26e73c (some changeg)
